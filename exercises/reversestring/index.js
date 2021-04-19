9b7d5e02061f4a831789ffb5c85c7f695fec4a45
Meredith <meredith@mcintosh.io>
2021-04-19 14:23:21 -0400

    other solutions for reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -14,3 +14,30 @@ function reverse(str) {
 }
 
 module.exports = reverse;
+
+
+//another way to solve the problem.. similar as above, but juse more concise
+// function reverse(str) {
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// another way to solve the problem using a for loop
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str) {
+//         reversed = character + reversed
+//     }
+
+//     return reversed
+// }
+
+//one more way to solve the problem
+// function reverse(str) {
+//    return str.split('').reduce((reversed, character) => {
+//         return character + reversed
+//     }, '');
+// }
